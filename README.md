2ad0efdb848802f39f350c0c6179a1d3dc8c0b3e
AlmirNET <almir.neto369@gmail.com>
2022-08-30 07:44:45 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# PROJETO-2-CALENDARIO-COM-IMG
+ Creating calendar to improve my skills in HTML 5, CSS3, javascript.
---
+++ sketch.js
@@ -0,0 +1,57 @@
+var h2 = document.querySelector(".calendar-picture h2");
+var h3 = document.querySelector(".calendar-picture h3");
+
+var monthArr = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "Decemeber",
+];
+
+var dayArr = [
+   "Sunday",
+   "Monday",
+   "Tuesday",
+   "Wednesday",
+   "Thrusday",
+   "Friday",
+   "Saturday",
+];
+
+var day31MonthArr = [
+    "January",
+    "March",
+    "May",
+    "July",
+    "August",
+    "October",
+    "December",
+]
+
+
+var d = new Date();
+
+var obj = getDate();
+
+function getDate(){
+    var month = d.getMonth();
+    month = monthArr[month]
+
+    var day = d.getDay();
+    day = dayArr[day];
+
+    var date = d.getDate();
+
+    h2.innerHTML = date+", "+day;
+    h3.innerHTML = month;
+
+    return {m:month, dy:day, dt:date, yr: d.getFullYear()}
+}
