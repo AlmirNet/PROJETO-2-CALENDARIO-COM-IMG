8575d161555caee6caa1845fd3c5c5911a3b12fb
AlmirNET <almir.neto369@gmail.com>
2022-08-30 11:16:08 -0300

    Creating calendar to improve my skills in HTML 5, CSS3, javascript.
    Creating calendar to improve my skills in HTML 5, CSS3, javascript.

--- sketch.js
+++ sketch.js
@@ -17,13 +17,13 @@ var monthArr = [
 ];
 
 var dayArr = [
-   "Sunday",
-   "Monday",
-   "Tuesday",
-   "Wednesday",
-   "Thrusday",
-   "Friday",
-   "Saturday",
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thrusday",
+  "Friday",
+  "Saturday",
 ];
 
 var day31MonthArr = [
@@ -34,24 +34,69 @@ var day31MonthArr = [
     "August",
     "October",
     "December",
-]
-
+];
 
 var d = new Date();
 
 var obj = getDate();
+generateCalendar();
 
 function getDate(){
-    var month = d.getMonth();
-    month = monthArr[month]
+  var month = d.getMonth();
+  month = monthArr[month]
 
-    var day = d.getDay();
-    day = dayArr[day];
+  var day = d.getDay();
+  day = dayArr[day];
 
-    var date = d.getDate();
+  var date = d.getDate();
 
-    h2.innerHTML = date+", "+day;
-    h3.innerHTML = month;
+  h2.innerHTML = date+", "+day;
+  h3.innerHTML = month;
 
-    return {m:month, dy:day, dt:date, yr: d.getFullYear()}
+  return {m:month, dy:day, dt:date, yr: d.getFullYear()}
+}
+
+function generateCalendar(){
+  var days;
+
+  if (obj.m === "February" && obj.yr % 4 !== 0) {
+    days = 28;
+  } else if (obj.m === "February" && obj.yr % 4 === 0) {
+    days = 29;
+  } else if (day31MonthArr.includes(obj.m)) {
+    days = 31;
+  } else {
+    days = 30;
+  }
+
+  var LocalDayArr = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thrusday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+
+  var startOfMonth= moment().clone().startOf("month").format("dddd")
+
+  var dayIndex = LocalDayArr.indexOf(startOfMonth);
+
+  for(var j=0;j<dayIndex;j++){
+    var element = document.createElement("div");
+    element.className = "calendar__number_empty";
+    document.getElementById("lc").appendChild(element);
+  }
+
+
+  for(var k = 1; k<=days;k++){
+      var element = document.createElement("div");
+      obj.dt === k 
+      ?(element.className = "calendar__number calendar__number--current")
+      :(element.className = "calendar__number");
+
+      element.appendChild(document.createTextNode(k));
+      document.getElementById("lc").appendChild(element);
+  }
 }
